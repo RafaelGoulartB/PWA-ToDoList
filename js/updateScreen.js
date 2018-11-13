01dbb1860929eb69200cd48942025ce149ff353f
Rafael Goulart <rafagoulartb@gmail.com>
2018-11-13 14:07:36 -0200

    Adapatações Mobile

--- js/updateScreen.js
+++ js/updateScreen.js
@@ -3,7 +3,7 @@ function updateScreen() {
     
     tasks.forEach(task => {
         list += '<div><button onclick="completeTask(this)" class="btn-complete" id='+task.id+'></button>'
-        list += "<li onclick='removeTask(this)' id="+task.id+">"+task.data.task+'</li>'; 
+        list += "<li id="+task.id+">"+task.data.task+'</li>'; 
         list += '<button onclick="removeTask(this)" class="btn-remover" id='+task.id+'>X</button></div>'
     });
 
