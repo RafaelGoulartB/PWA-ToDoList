d046e6961d96f443ede7646b6f0fc0a50b99a88b
RafaelGoulartB <rafagoulartb@gmail.com>
2020-02-07 09:31:06 -0300

    PWA

---
+++ sw.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/css/',
+        '/js/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png',
+        '/css/countTasks.css',
+        '/css/index.css',
+        '/css/input.css',
+        '/css/reset.css',
+        '/css/tasks.css',
+        '/js/taskFactory.js',
+        '/js/taskStorage.js',
+        '/js/updateScreen.js',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
